2c242ad309979d7c21da2963c5c11a83c2ec9ee4
Craig Peterson <cpeterson@stackoverflow.com>
2018-05-02 15:51:32 -0400

    aaa

--- q/index.js
+++ q/index.js
@@ -5,11 +5,13 @@ module.exports = function (context, req) {
     var checks = req.body.checks;
     context.log(checks.length);
     var pending = checks.length;
+    var answers = Array(pending).fill(0);
 
     for (var i = 0; i<checks.length; i++){
         var ck = checks[i];
-        makeReq(ck.name, ck.addr, context,i, function(idx,time){
+        makeReq(ck.name, ck.addr, context,i, function(idx,result,time){
             context.log(`Finished query ${idx}: ${time.toString()}ms`);
+            answers[i] = {time:time.toString(),v:result}
             pending--;
             if (pending == 0){
                 context.done();
@@ -37,20 +39,24 @@ function makeReq(name, target, context, idx, callback){
         cache: false,
     });
 
+    var result = "?"
+
     dnsReq.on('timeout', function () {
+        result = "timeout"
         context.log(`Timeout in making request for ${name} from ${target}`);
     });
 
     dnsReq.on('message', function (err, answer) {
-        
+        context.log(err, answer)
         answer.answer.forEach(function (a) {
+            result = a.address;
             context.log(`${target} replies ${a.address} for ${name}`);
         });
     });
 
     dnsReq.on('end', function () {
         var delta = (Date.now()) - start;
-        callback(idx, delta);
+        callback(idx,result, delta);
     });
     dnsReq.send();
 }
